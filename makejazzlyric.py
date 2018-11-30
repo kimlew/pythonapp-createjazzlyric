54b67849229f03e00f6a8a4013dfb18340395ed0
Kim Lew <kimlew451@gmail.com>
2018-11-30 13:25:43 -0700

    Commented out connDict for now

--- makejazzlyric.py
+++ makejazzlyric.py
@@ -10,5 +10,5 @@ def entry_page() -> 'html':
                            the_title='Make a Jazz Lyric Web App')
 
 if __name__ == '__main__':
-    app.config['dbconfig'] = connDict
+    #  app.config['dbconfig'] = connDict
     app.run(debug=True)
