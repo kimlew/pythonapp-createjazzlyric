6106a2fda7798508f033200489730d22279e2162
Kim Lew <kimlew451@gmail.com>
2018-11-30 09:55:46 -0700

    Changed file name

---
+++ makejazzlyric.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title='Make a Jazz Lyric Web App')
+
+if __name__ == '__main__':
+    app.config['dbconfig'] = connDict
+    app.run(debug=True)
