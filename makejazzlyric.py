c1a7e5a2a337d6ade35708fd3ae99a21e057e8d1
Kim Lew <kimlew451@gmail.com>
2018-12-11 13:32:34 -0700

    Added another part to prep for vowel2

--- makejazzlyric.py
+++ makejazzlyric.py
@@ -22,7 +22,8 @@ def create_lyric() -> str:
     vowel1 = request.form['vowel1']
 
     lyric = "Sh" + (vowel1*2) +\
-            " b" + (vowel1*4) +\
+            " b" + (vowel1*4) + \
+            " d" + (vowel1 * 3) + \
             " z" + "eee" +\
             " d" + (vowel1*9) + "..."
     return render_template('results.html', the_title=title, the_vowel1=vowel1, the_lyric=lyric,)
