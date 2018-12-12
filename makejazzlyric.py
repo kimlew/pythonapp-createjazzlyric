26bd725142909ed43c6597d6bd7518551828ae51
Kim Lew <kimlew451@gmail.com>
2018-12-11 19:37:53 -0700

    Added spaces around operators so more readable

--- makejazzlyric.py
+++ makejazzlyric.py
@@ -22,11 +22,11 @@ def create_lyric() -> str:
     vowel1 = request.form['vowel1']
     vowel2 = request.form['vowel2']
 
-    lyric = "Sh" + (vowel1*2) +\
+    lyric = "Sh" + (vowel1 * 2) +\
             " b" + ("oop") + \
             " d" + (vowel2 * 2) + \
             " z" + "eeee" +\
-            " d" + (vowel1*7) + "..."
+            " d" + (vowel1 * 7) + "..."
     return render_template('results.html', the_title=title, the_vowel1=vowel1, the_vowel2=vowel2, the_lyric=lyric,)
 
 
