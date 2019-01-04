085b19d3202b7cac2cae23a5a21cd496ad84a234
Kim Lew <kimlew451@gmail.com>
2019-01-04 12:48:23 -0700

    Removed comment & changed double quotes to single ones

--- createjazzlyric.py
+++ createjazzlyric.py
@@ -29,15 +29,14 @@ def create_lyric() -> str:
     vowel2_amount = (int) (request.form['vowel2_amount'])
     consonant = request.form['consonant'].lower()
 
-    # TODO:  Add the_msg_params=msg_params
-    need_vowel_msg = "Enter a vowel."
-    need_number_msg = "Enter a number from 3-9."
-    need_consonant_msg = "Enter a consonant."
+    need_vowel_msg = 'Enter a vowel.'
+    need_number_msg = 'Enter a number from 3-9.'
+    need_consonant_msg = 'Enter a consonant.'
 
     msg_params = {
-        "need_vowel_msg": need_vowel_msg,
-        "need_number_msg": need_number_msg,
-        "need_consonant_msg": need_consonant_msg
+        "need_vowel_msg" : need_vowel_msg,
+        "need_number_msg" : need_number_msg,
+        "need_consonant_msg" : need_consonant_msg
     }
 
     if vowel1 in vowel_set:
