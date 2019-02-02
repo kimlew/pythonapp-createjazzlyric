a9af1b40859ffa34cc89b1c93f93505274e82a89
Kim Lew <kimlew451@gmail.com>
2019-02-02 15:56:21 -0700

    Added cursor.fetchall() - to fetch data from the result set from the query

--- createjazzlyric.py
+++ createjazzlyric.py
@@ -144,6 +144,7 @@ def create_song() -> str:
             EXTRACT(DAY FROM date_created) 
             FROM lyric WHERE date_deactivated IS NULL"""
 
+        all_lyrics = cursor.fetchall()
 
     finally:
         conn.close()
